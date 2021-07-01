54f78bb77ce95049a5a21ec720bf48cd0b2db808
Obada <obadamoustafa@gmail.com>
2021-07-01 15:58:09 +0200

    some js exersices

---
+++ JS/week2/even-odd-reporter.js
@@ -0,0 +1,16 @@
+/**
+ * Report whether or not a number is odd/even!
+ *
+ *  Create a for loop, that iterates from 0 to 20.
+ *  Create a conditional statement that checks if the value of the counter variable is odd or even.
+ *  If it's odd, log to the console The number [PUT_NUMBER_HERE] is odd!.
+ *  If it's even, log to the console The number [PUT_NUMBER_HERE] is even!.
+ */
+
+for (var num = 0 ; num <= 20 ; num++){
+    if (num %2 === 0){
+        console.log("The number " + num + " is even!")
+    } else {
+        console.log("The number " + num + " is odd!")
+    }
+}
