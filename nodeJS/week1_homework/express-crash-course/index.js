014c0647783ea3d88626ce91f5617ea1a456bdac
Obada <obadamoustafa@gmail.com>
2021-09-20 18:07:14 +0200

    made a small porject with express module

--- nodeJS/week1_homework/express-crash-course/index.js
+++ nodeJS/week1_homework/express-crash-course/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const path = require('path');
+const router = require('./routes/api/members');
+
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('hello world')
-})
+// app.use(logger);
+
+// body parser middleware
+app.use(express.json());
+// app.use(express.urlencoded({extended: false}))
+
+
+// set a static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// members api route
+app.use('/api/members', router)
+
 
 const PORT = process.env.PORT || 5000 ;
-app.listen(PORT, ()=>console.log(`server started on ${PORT}`);)
+app.listen(PORT, ()=>console.log(`server started on ${PORT}`));
