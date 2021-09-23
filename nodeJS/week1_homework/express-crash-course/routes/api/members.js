8f15536b2120c893f11428dd69f6795e05d0b759
Obada <obadamoustafa@gmail.com>
2021-09-23 18:17:00 +0200

    want to push

--- nodeJS/week1_homework/express-crash-course/routes/api/members.js
+++ nodeJS/week1_homework/express-crash-course/routes/api/members.js
@@ -46,6 +46,18 @@ router.post('/', (req, res) => {
 })
 
 //update members
+router.put('/:id', (req, res) => {
+    const found = members.some( member => `${member.id}` === req.params.id )
+    if (found) {
+        res.json(members.filter((member)=> `${member.id}` === req.params.id))
+    } else {
+        res.status(400)
+        .json({error: {
+            code: 404,
+            message: `we don't have a member with this id : ${req.params.id} `
+        }});
+    }
+})
 
 
 module.exports = router;
