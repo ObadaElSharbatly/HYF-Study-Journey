b0e98ec4e1f5c73e76000113c19bf11ae2c5a900
Obada <obadamoustafa@gmail.com>
2021-11-01 15:19:24 +0100

    level 1 babyNames

--- React/Baby_Name_Picker/src/App.js
+++ React/Baby_Name_Picker/src/App.js
@@ -3,15 +3,18 @@ import babyNames from './baby_names.json';
 import NameLabel from './components/name_label';
 
 const sortedBabyNames = babyNames.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
-console.log(sortedBabyNames);
 
 function App() {
+  console.log(sortedBabyNames);
   return (
     <div className="App">
-      <NameLabel />
-      {/* {sortedBabyNames.map((baby, i) => {
-        return <NameLabel baby={baby} key={i}/>
-      })} */}
+      <div className="names-con">
+        {
+          sortedBabyNames.map((baby, i) => {
+            return <NameLabel baby={baby} key={i}/>
+          })
+        }
+      </div>
     </div>
   );
 }
---
+++ React/Baby_Name_Picker/src/components/name_label.js
@@ -0,0 +1,15 @@
+import boyLogo from '../img/boy-icon.png';
+import girlLogo from '../img/girl-icon.png';
+
+function NameLabel ({baby}) {
+    const boy = baby.sex === "m";
+
+    return (
+        <div className="label" style={boy ? {"backgroundColor": "#6E91D7" , "color": "#15284D"} : {"backgroundColor": "pink", "color": "#830B79"}}>
+            <img src={boy ? boyLogo : girlLogo} alt={boy ? "boy logo" : "girl logo"} width="20" height="20" />
+            <p>{baby.name}</p>
+        </div>
+    )
+}
+
+export default NameLabel;
