7cebb22aaf9351760ac141089b2fea09373707d8
Obada <obadamoustafa@gmail.com>
2021-11-02 14:23:49 +0100

    baby names challange completed successfully

--- React/Baby_Name_Picker/src/components/fav.js
+++ React/Baby_Name_Picker/src/components/fav.js
@@ -1,15 +1,17 @@
 import NameLabel from "./name_label";
-import { favNames } from "./search-bar";
+// import { favNames } from "./search-bar";
 
-function FavList () {
-    
-    
+function FavList ({favNames, rmFav}) {
     return (
         <div className="fav-con">
             <p>Click on your fav names to add to fav list</p>
             <div className="fav">
                 {favNames.map((baby, i) => {
-                    return <NameLabel baby={baby} key={i}/>
+                    return (
+                        <div onClick={rmFav} key={i}>
+                            <NameLabel baby={baby} />
+                        </div>
+                    )
                 })}
             </div>
             <hr/>
--- React/Baby_Name_Picker/src/components/search-bar.js
+++ React/Baby_Name_Picker/src/components/search-bar.js
@@ -5,38 +5,48 @@ import { useState } from "react";
 
 
 const sortedBabyNames = babyNames.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
-export const favNames = [];
 
 function SearchBar () {
-    const [babies, setBabies] = useState(sortedBabyNames);  
+    const [babies, setBabies] = useState(sortedBabyNames);
+    const [favNames, setFavNames] = useState([]);
+    console.log("favNames:" , favNames);
 
     const filterNames = e => {
+        const favNamesSt = favNames.map(baby => baby.name);
         const kidName = e.target.value;
-        setBabies(kidName === "" ? sortedBabyNames : sortedBabyNames.filter(baby => 
+        setBabies(kidName === "" ? sortedBabyNames.filter(baby => !favNamesSt.includes(baby.name)) : babies.filter(baby => 
             baby.name.toLowerCase().includes(kidName.toLowerCase())))
     }
 
-    const addRemoveFromFav = (e) => {
+    const addToFav = (e) => {
         const babyID = e.target.id !== "" ? e.target.id : e.target.parentElement.id
-        const selectedBaby = babies.find( baby => baby.id === Number(babyID) )
-        const selectedBabyIndex = babies.indexOf(selectedBaby);
-        console.log("index:", selectedBabyIndex);
-
-        favNames.push(selectedBaby);
-        console.log("favNames:", favNames);
+        const selectedBaby = sortedBabyNames.find( baby => baby.id === Number(babyID) )
+        const allFavNames = [...favNames, selectedBaby]
+        setFavNames(allFavNames)
 
         setBabies( babies.filter(baby => baby.id !== selectedBaby.id) );
-        console.log("babies:", babies);
+    }
+
+    function rmFromFav (e) {
+        console.log(e);
+        const babyID = e.target.id !== "" ? e.target.id : e.target.parentElement.id
+        const selectedBaby = sortedBabyNames.find( baby => baby.id === Number(babyID) )
+        
+        setFavNames(favNames.filter(baby => baby.id !== selectedBaby.id))
+        console.log("favNames", favNames);
+
+        const mainBabyList = [...babies, selectedBaby].sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
+        setBabies(mainBabyList)
     }
 
     return (
         <div className="App">
             <input type="text" onChange={filterNames} placeholder="search my kid's name" className="search-bar"/>
-            <FavList />
+            <FavList favNames={favNames} rmFav={rmFromFav}/>
             <div className="names-con">
                 {
                     babies.map((baby, i) => {
-                        return <NameLabel baby={baby} key={i} addFav={addRemoveFromFav} />
+                        return <NameLabel baby={baby} key={i} addFav={addToFav} />
                     })
                 }
             </div>
